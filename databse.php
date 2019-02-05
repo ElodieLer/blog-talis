459b001ef13cab11a23219898495416077629d4d
ElodieLer <elogood@hotmail.fr>
2019-02-05 10:07:07 +0100

    modif3

--- databse.php
+++ databse.php
@@ -9,7 +9,6 @@ try { //fonction de connexion
     $db = new PDO("musql:host=$servername;dbname=myDB"), $username, $password)
     // set the PDO error mode to exception
     $db->setAttribute(PDO: :ATTR_ERRMODE, PDO: :ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 }catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
 }
--- login_post.php
+++ login_post.php
@@ -1,4 +1,22 @@
 <?php
+require "database.php";
+
+$req = $db->prepare('SELECT * FROM users WHERE pseudo = :pseudo AND password = :password'); //on ecrit notre requete
+$req->execute(arrway(                   //on execute notre requete
+    "pseudo" => $_POST["pseudo"],
+    "password" => $_POST["password"]
+)); 
+$result = $req->fetch();
+if(!$result){
+    header("Location: login.php");
+}else{
+    session_start();
+    $_SESSION["pseudo"] = $_POST["pseudo"];
+    header("Location: login.php");
+}
+
+
+// Si pseudo égal à admin et password égal à admin
 if($_POST["pseudo"] === "admin" && $_POST["password"] === "admin"){
     session_start();
     $_SESSION["pseudo"] = $_POST["pseudo"];
